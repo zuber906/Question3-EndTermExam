45d41eb4405af2dd0cd037361d181d0469f588fd
meer <meer@DESKTOP-1H02SQI>
2019-08-14 19:04:55 +0530

    commit

--- src/test/java/main/MediumQuestionTest.java
+++ src/test/java/main/MediumQuestionTest.java
@@ -8,15 +8,15 @@ public class MediumQuestionTest {
 	
 	Customer previlaged=new PrivilegeCustomer("venkat", "coimbatore",21, "9600511879");
 	Customer senior=new SeniorCitizenCustomer("venkat", "coimbatore",21, "9600511879");
-	@SuppressWarnings("static-access")
+	//@SuppressWarnings("static-access")
 	@Test
 	public void privilegeCustomer() {
 		assertEquals(700.0,previlaged.getBillAmount(1000.0),0.0);
 	}
-	@SuppressWarnings("static-access")
+	//@SuppressWarnings("static-access")
 	@Test
 	public void seniorCitizen() {
-		assertEquals(440.0,senior.getBillAmount(500.0),0.0);
+		assertEquals(450.0,senior.getBillAmount(500.0),0.0);
 	}
 
 }
